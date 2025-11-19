e292a7297e962a95ab9293c2e679edbcde0f3ace
Carolina Lagoria <lagoriacarolina@gmail.com>
2025-11-19 18:06:20 -0300

    Actividad 1, creacion de señales de 12 bit y el vector de muestras

---
+++ LAB_4/LAB_4_2025.m
@@ -0,0 +1,37 @@
+clear all
+close all
+
+
+fs = 153.6e3;         % Frecuencia de muestreo de la señal modulada 156.6 kHz
+k = (0:11)';          % Índices de los 12 canales 
+f_k = (38.7 + 0.15 * k) * 1e3; % Frecuencias de portadora en Hz 
+delta_f = 0.15e3;     % Separación de frecuencia (150 Hz) entre canales
+Tsym = 1 / delta_f;   % Período de símbolo (~6.667 ms)
+
+N = round(Tsym * fs); % cantidad de muestras de la señal portadora (1024)
+t = (0:N-1) / fs; % Vector de tiempo para un símbolo
+
+CH = 4;               % n° de canales  de 12 bits
+
+%% Actividad 1
+
+% Costruccion de señales de 12 bit en forma de columna
+L = 10;               % muestras por canal
+
+ch1 = round(linspace(0, 4095, L))';   % Canal 1: Rampa lineal (de 0 a 4095)
+
+ch2 = mod(round(linspace(0, 8190, L))', 4096);  % Canal 2: Diente de sierra 
+
+ch3 = ones(L, 1) * 2048;   % Canal 3: Constante (valor medio)
+
+ch4 = randi([0, 4095], L, 1);  % Canal 4: Aleatoria (valores 12 bit)
+
+
+bin12 = cell(L, CH);  % matriz de celdas 10x4
+
+for l = 1:L
+    muestras_actuales = [ch1(l), ch2(l), ch3(l), ch4(l)];
+    for ch = 1:CH
+        bin12{l, ch} = dec2bin(muestras_actuales(ch), 12);
+    end
+end
